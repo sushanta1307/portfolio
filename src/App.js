b416866a5a65494d8187e25e0726bccb6eab8e8d
= <=>
2022-08-14 22:45:50 +0530

    BAsic Portfolio ready for deployment

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState, useMemo, useRef, useEffect } from "react";
 import "./App.css";
 import { styled } from "@mui/material/styles";
 import Paper from "@mui/material/Paper";
@@ -21,6 +22,10 @@ import Divider from "@mui/material/Divider";
 
 import GitHubIcon from "@mui/icons-material/GitHub";
 import TouchAppIcon from "@mui/icons-material/TouchApp";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import EmailIcon from "@mui/icons-material/Email";
+import LocalPhoneIcon from "@mui/icons-material/LocalPhone";
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
@@ -30,11 +35,48 @@ const Item = styled(Paper)(({ theme }) => ({
   color: theme.palette.text.secondary,
 }));
 
+function useIsInViewport(ref) {
+  const [isIntersecting, setIsIntersecting] = useState(false);
+
+  const observer = useMemo(
+    () =>
+      new IntersectionObserver(([entry]) =>
+        setIsIntersecting(entry.isIntersecting)
+      ),
+    []
+  );
+
+  useEffect(() => {
+    observer.observe(ref.current);
+
+    return () => {
+      observer.disconnect();
+    };
+  }, [ref, observer]);
+
+  return isIntersecting;
+}
+
 function App() {
+  const ref1 = useRef(null);
+  const ref2 = useRef(null);
+  const ref3 = useRef(null);
+  const ref4 = useRef(null);
+  const ref5 = useRef(null);
+  const ref6 = useRef(null);
+  const ref7 = useRef(null);
+
+  const isInViewport1 = useIsInViewport(ref1);
+  const isInViewport2 = useIsInViewport(ref2);
+  const isInViewport3 = useIsInViewport(ref3);
+  const isInViewport4 = useIsInViewport(ref4);
+  const isInViewport5 = useIsInViewport(ref5);
+  const isInViewport6 = useIsInViewport(ref6);
+  const isInViewport7 = useIsInViewport(ref7);
   return (
     <div className="App">
       <Grid container spacing={2}>
-        <Grid item xs={1} sm={2}>
+        <Grid item xs={2} sm={2} className="sidebar">
           <Item>
             <Avatar
               alt="Sushanta Senapati"
@@ -44,52 +86,55 @@ function App() {
             />
           </Item>
           <Item className="itemStyle">
-            <a>
+            <a href="#about" className={isInViewport1 ? "active" : ""}>
               <InfoIcon />
               <span>About</span>
             </a>
           </Item>
           <Item className="itemStyle">
-            <a>
-            <ShowChartSharpIcon />
-            <span>Experience</span>
+            <a href="#experience" className={isInViewport2 ? "active" : ""}>
+              <ShowChartSharpIcon />
+              <span>Experience</span>
             </a>
           </Item>
           <Item className="itemStyle">
-            <a>
-            <SchoolIcon />
-            <span>Education</span>
+            <a href="#projects" className={isInViewport3 ? "active" : ""}>
+              <TaskIcon />
+              <span>Projects</span>
             </a>
           </Item>
           <Item className="itemStyle">
-            <a>
-            <TaskIcon />
-            <span>Projects</span>
+            <a href="#education" className={isInViewport4 ? "active" : ""}>
+              <SchoolIcon />
+              <span>Education</span>
             </a>
           </Item>
           <Item className="itemStyle">
-            <a>
-            <AbcIcon />
-            <span>Skills</span>
+            <a href="#skills" className={isInViewport5 ? "active" : ""}>
+              <AbcIcon />
+              <span>Skills</span>
             </a>
           </Item>
           <Item className="itemStyle">
-            <a>
-            <ContactsIcon />
-            <span>Contact</span>
+            <a href="#contact" className={isInViewport6 ? "active" : ""}>
+              <ContactsIcon />
+              <span>Contact</span>
             </a>
           </Item>
           <Item className="itemStyle">
-            <a>
-            <ArticleIcon />
-            <span>Resume</span>
+            <a href="#resume" className={isInViewport7 ? "active" : ""}>
+              <ArticleIcon />
+              <span>Resume</span>
             </a>
           </Item>
         </Grid>
-        <Grid item xs={11} sm={10} className="grid2">
-          {/* <Item></Item> */}
-          <Item id="about">
-            <h1>About</h1>
+      </Grid>
+
+      <Grid container spacing={2}>
+        <Grid item xs={2} sm={2}></Grid>
+        <Grid item xs={10} className="grid2">
+          <h1>About</h1>
+          <Item ref={ref1} id="about">
             <p>
               I am Sushanta Senapati, currently a final year student at National
               Institute of Technology, Rourkela, pursuing my undergraduate
@@ -99,7 +144,7 @@ function App() {
               ReactJs, NodeJs, MongoDB, ExpreessJs, Python, C++. etc.
             </p>
             <p>
-              <b>Languages: </b>C, C++, Pyhton, JavaScript
+              <b>Languages: </b>C, C++, Python, JavaScript
             </p>
             <p>
               <b>Databases: </b>MySql, MongoDB
@@ -108,11 +153,12 @@ function App() {
               <b>Technologies: </b>Git, JIRA
             </p>
           </Item>
-          <Item id="experience">
-            <h1>Experience</h1>
+          <h1>Experience</h1>
+          <Item ref={ref2} id="experience">
             <Card
-              sx={{ minWidth: 275 }}
+              sx={{ width: "100%" }}
               style={{ backgroundColor: "lightgreen" }}
+              className="exp-card"
             >
               <CardContent>
                 <Typography>
@@ -125,18 +171,10 @@ function App() {
                 <Typography>
                   <h2>Server Technology Intern</h2>
                   <p>
-                    Lorem Ipsum is simply dummy text of the printing and
-                    typesetting industry. Lorem Ipsum has been the industry's
-                    standard dummy text ever since the 1500s, when an unknown
-                    printer took a galley of type and scrambled it to make a
-                    type specimen book
+                    Worked with Site Reliable Engineers of Block Storage Team of Oracle in developing the BOSS Client script
                   </p>
                   <p>
-                    Lorem Ipsum is simply dummy text of the printing and
-                    typesetting industry. Lorem Ipsum has been the industry's
-                    standard dummy text ever since the 1500s, when an unknown
-                    printer took a galley of type and scrambled it to make a
-                    type specimen book
+                    It will automate the works of BOSS UI, hence reducing the effort to do works manually for block storage works like managing block volumes, boot volumes, backups, tenant utilizations. etc
                   </p>
                 </Typography>
                 <Typography style={{ display: "flex", padding: "20px" }}>
@@ -146,7 +184,11 @@ function App() {
                 </Typography>
               </CardContent>
             </Card>
-            <Card sx={{ minWidth: 275 }} style={{ backgroundColor: "hotpink" }}>
+            <Card
+              sx={{ width: "100%" }}
+              style={{ backgroundColor: "hotpink" }}
+              className="exp-card"
+            >
               <CardContent>
                 <Typography>
                   <img
@@ -158,18 +200,10 @@ function App() {
                 <Typography>
                   <h2>Project Intern</h2>
                   <p>
-                    Lorem Ipsum is simply dummy text of the printing and
-                    typesetting industry. Lorem Ipsum has been the industry's
-                    standard dummy text ever since the 1500s, when an unknown
-                    printer took a galley of type and scrambled it to make a
-                    type specimen book
+                    Worked on open source library Development using extended typescript on Rounded Image View for android 
                   </p>
                   <p>
-                    Lorem Ipsum is simply dummy text of the printing and
-                    typesetting industry. Lorem Ipsum has been the industry's
-                    standard dummy text ever since the 1500s, when an unknown
-                    printer took a galley of type and scrambled it to make a
-                    type specimen book
+                    Can use the library for making images to different shapes especially rounded.
                   </p>
                 </Typography>
                 <Typography style={{ display: "flex", padding: "20px" }}>
@@ -180,69 +214,229 @@ function App() {
               </CardContent>
             </Card>
           </Item>
-          <Item id="projects">
-            <h1>Projects</h1>
-            <div style={{ display: "flex" }}>
-              <Card sx={{ width: "31%", margin: "1%" }}>
-                <CardMedia
-                  component="img"
-                  height="240"
-                  image="/images/coding.gif"
-                  alt="Webdev"
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="div">
-                    e-Commerce
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    A shopping app for Android to make your shopping easy.
-                  </Typography>
-                </CardContent>
-                <CardActions>
+          <h1>Projects</h1>
+          <Item
+            ref={ref3}
+            id="projects"
+            style={{ display: "flex", flexDirection: "row" }}
+          >
+            <Card sx={{ width: "31%", margin: "1%" }}>
+              <CardMedia
+                component="img"
+                height="240"
+                image="/images/coding.gif"
+                alt="Webdev"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  e-Commerce
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  A shopping app for Android to make your shopping easy.
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <a href="https://github.com/sushanta1307/e-commerce">
                   <GitHubIcon />
+                </a>
+                <a href="#">
                   <TouchAppIcon />
-                </CardActions>
-              </Card>
-              <Card sx={{ width: "31%", margin: "1%" }}>
-                <CardMedia
-                  component="img"
-                  height="240"
-                  image="/images/weather.gif"
-                  alt="Webdev"
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="div">
-                    Weather App
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    A weather prediction app for a week of any location.
-                  </Typography>
-                </CardContent>
-                <CardActions>
+                </a>
+              </CardActions>
+            </Card>
+            <Card sx={{ width: "31%", margin: "1%" }}>
+              <CardMedia
+                component="img"
+                height="240"
+                image="/images/weather.gif"
+                alt="Webdev"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Weather App
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  A weather prediction app for a week of any location.
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <a href="https://github.com/sushanta1307/weather-app">
                   <GitHubIcon />
+                </a>
+                <a href="#">
                   <TouchAppIcon />
-                </CardActions>
-              </Card>
-              <Card sx={{ width: "31%", margin: "1%" }}>
-                <CardMedia
-                  component="img"
-                  height="240"
-                  image="/images/reading.gif"
-                  alt="Webdev"
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="div">
-                    Blog App
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    A blog app where you can read and create your own blogs
-                  </Typography>
-                </CardContent>
-                <CardActions>
+                </a>
+              </CardActions>
+            </Card>
+            <Card sx={{ width: "31%", margin: "1%" }}>
+              <CardMedia
+                component="img"
+                height="240"
+                image="/images/reading.gif"
+                alt="Webdev"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Blog App
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  A blog app where you can read and create your own blogs
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <a href="https://github.com/sushanta1307/blogapp">
                   <GitHubIcon />
+                </a>
+                <a href="#">
                   <TouchAppIcon />
-                </CardActions>
-              </Card>
+                </a>
+              </CardActions>
+            </Card>
+          </Item>
+          <h1>Education</h1>
+          <Item
+            id="education"
+            ref={ref4}
+            style={{ display: "flex", flexDirection: "row" }}
+          >
+            <Card sx={{ width: "48%", margin: "1%" }}>
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Jawahar Navoday Vidyalaya, Dhenkanal
+                  <br />
+                  Odisha 2017-2019
+                  <Divider />
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  <ul>
+                    <li>
+                      <b>Stream: </b>Science
+                    </li>
+                    <li>
+                      <b>Percentage: </b>96.8%
+                    </li>
+                  </ul>
+                  <ul>
+                    <p className="courses">
+                      <b>Relevant Courseworks:</b>
+                      <ul>
+                        <li>Physics</li>
+                        <li>Chemistry</li>
+                        <li>Mathematics</li>
+                        <li>Biology</li>
+                        <li>English</li>
+                      </ul>
+                    </p>
+                  </ul>
+                </Typography>
+              </CardContent>
+            </Card>
+            <Card sx={{ width: "48%", margin: "1%" }}>
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  National Institute of Technology, Rourkela
+                  <br />
+                  Odisha 2019-2023
+                  <Divider />
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  <ul>
+                    <li>
+                      <b>Stream: </b>Computer Science and Engineering
+                    </li>
+                    <li>
+                      <b>CGPA: </b>9.34
+                    </li>
+                  </ul>
+                  <ul>
+                    <p className="courses">
+                      <b>Relevant Courseworks:</b>
+                      <ul>
+                        <li>Data Structures and Algorithms</li>
+                        <li>Cloud Computing</li>
+                        <li>Computer Networks</li>
+                        <li>Operating System</li>
+                        <li>Database Engineering</li>
+                      </ul>
+                    </p>
+                  </ul>
+                </Typography>
+              </CardContent>
+            </Card>
+          </Item>
+          <h1>Skills</h1>
+          <Item id="skills" ref={ref5}>
+            <Card sx={{ width: "100%" }}>
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Languages and Databases
+                  <Divider />
+                </Typography>
+                <div
+                  className="logo"
+                  style={{
+                    diaplay: "flex",
+                    flexDirection: "row",
+                    flexWrap: "wrap",
+                  }}
+                >
+                  <img src="/images/python.jpg" alt="Python" />
+                  <img src="/images/mysql.jpg" alt="Mysql" />
+                  <img src="/images/cpp.png" alt="C++" />
+                  <img src="/images/mongoDB.png" alt="MongoDB" />
+                </div>
+              </CardContent>
+            </Card>
+            <Card sx={{ width: "100%" }}>
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Softwares
+                  <Divider />
+                </Typography>
+                <div
+                  className="logo"
+                  style={{
+                    diaplay: "flex",
+                    flexDirection: "row",
+                    flexWrap: "wrap",
+                  }}
+                >
+                  <img src="/images/git.png" alt="Git" />
+                  <img src="/images/matlab.png" alt="Matlab" />
+                  <img src="/images/vscode.png" alt="VS Code" />
+                </div>
+              </CardContent>
+            </Card>
+          </Item>
+          <h1>Contact</h1>
+          <Item id="contact" ref={ref6}>
+            <a href="#">
+              <LocalPhoneIcon /> +91 7609067900
+            </a>
+            <a href="mailto:1307senapati@gmail.com" target="_blank">
+              <EmailIcon />
+              1307senapati@gmail.com
+            </a>
+            <a href="https://www.linkedin.com/in/sushanta-senapati-b089801b4/" target="_blank">
+              <LinkedInIcon />
+              Sushanta Senapati
+            </a>
+            <a href="https://www.instagram.com/_sushanta.senapati_/" target="_blank">
+              <InstagramIcon />
+              Sushanta Senapati
+            </a>
+            <a href="https://github.com/sushanta1307/" target="_blank">
+              <GitHubIcon />
+              Sushanta Senapati
+            </a>
+          </Item>
+          <h1>Resume</h1>
+          <Item id="resume" ref={ref7}>
+            <div>
+              <img src="/images/resume.png" alt="Resume" />
+              <a href="/Sushanta Senapati Resume.pdf" download>
+                Download Resume
+              </a>
             </div>
           </Item>
         </Grid>
